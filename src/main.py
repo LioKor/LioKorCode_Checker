027a0794fc3c12dc630f9a4d6782649929981551
KoroLion <korolion31@yandex.ru>
2022-03-12 18:12:28 +0300

    feature: http api with flask

--- src/main.py
+++ src/main.py
@@ -1,6 +1,12 @@
+from flask import Flask, request, Response
+import json
+
 from task_checker import check_task
 
-example_source_code = '''#include "stdio.h"
+app = Flask(__name__)
+
+example_request = {
+    'sourceCode': '''#include "stdio.h"
 int main() {
     int a, b;
 
@@ -9,12 +15,25 @@ int main() {
     printf("%d", a + b);
     
     return 0;
-}'''
+}''',
+    'tests': [
+        ['1 2', '3'],
+        ['4 5', '9'],
+        ['-2 2', '0']
+    ]
+}
 
-example_tests = [
-    ['1 2', '3'],
-    ['4 5', '9'],
-    ['-2 2', '0']
-]
 
-print(check_task(example_source_code, example_tests))
+@app.route('/check_task/long', methods=['POST'])
+def check_task_long_view():
+    task = request.json
+    try:
+        result = check_task(task['sourceCode'], task['tests'])
+        return Response(json.dumps(result), mimetype='application/json')
+    except Exception as e:
+        return Response('{}', status=500, mimetype='application/json')
+
+
+app.run(host='0.0.0.0', port=7070)
+
+print(json.dumps(example_request))
