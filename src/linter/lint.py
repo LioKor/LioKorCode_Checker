6f5f1856cdb7459c6a18c6d945b9cc2e3ebd587d
KoroLion <korolion31@yandex.ru>
2022-05-30 10:15:25 +0300

    linter: update

--- src/linter/lint.py
+++ src/linter/lint.py
@@ -1,19 +1,8 @@
-c_cpp = '''
-#include <stdio.h>
-
-int main() {
-    int a,b;
-    if (a > b) {
-        printf("TRUE");
-    } else {
-        printf("FALSE");
-    }
-}
-'''
+import unittest
 
 
 def lint_c_or_cpp(code: str):
-    errors = ''
+    errors = []
 
     indent_level = 0
     space_length = None
@@ -28,26 +17,46 @@ def lint_c_or_cpp(code: str):
 
         current_indent = 0
         indent_symbol = None
-        need_check = True
+        need_indentation_check = True
+        checking_indentation = True
         for i in range(0, len(line)):
             c = line[i]
-            if c != '\t' and c != ' ':
-                if space_length is None and indent_symbol == ' ' and indent_level > 0:
-                    space_length = current_indent / indent_level
-                break
-            if indent_symbol is not None and c != indent_symbol:
-                need_check = False
-                errors += 'Line {}: Mixing tab and spaces is a bad practice\n'.format(line_number)
-                break
-            indent_symbol = c
-            current_indent += 1
 
-        if need_check:
+            if checking_indentation:
+                if c != '\t' and c != ' ':
+                    if space_length is None and indent_symbol == ' ' and indent_level > 0:
+                        space_length = current_indent / indent_level
+                    checking_indentation = False
+                    continue
+                if indent_symbol is not None and c != indent_symbol:
+                    need_indentation_check = False
+                    errors.append({
+                        'line': line_number,
+                        'error': 'indentation/mix'
+                    })
+                    break
+                indent_symbol = c
+                current_indent += 1
+            elif c == ',':
+                l = len(line)
+                before_space = i > 0 and line[i - 1] == ' '
+                no_space = i < l - 1 and line[i + 1] != ' '
+                extra_space = i < l - 2 and line[i + 2] == ' '
+                if before_space or no_space or extra_space:
+                    errors.append({
+                        'line': line_number,
+                        'error': 'spaces/punctuation'
+                    })
+
+        if need_indentation_check:
             expected_indent = indent_level
             if indent_symbol == ' ' and space_length is not None:
                 expected_indent *= space_length
-            if current_indent != expected_indent and need_check:
-                errors += 'Line {}: incorrect indentation or curly braces missing\n'.format(line_number)
+            if current_indent != expected_indent and need_indentation_check:
+                errors.append({
+                    'line': line_number,
+                    'error': 'indentation/bad'
+                })
 
         has_new_block = line.find('{') != -1
         if has_new_block:
@@ -56,4 +65,36 @@ def lint_c_or_cpp(code: str):
     return errors
 
 
-print(lint_c_or_cpp(c_cpp))
+class LinterTest(unittest.TestCase):
+    def test_lint_files(self):
+        test_cases = [
+            ['test_cases/c_good_file.c', []],
+            ['test_cases/c_almost_good_file.c', [
+                {'line': 4, 'error': 'spaces/punctuation'},
+                {'line': 6, 'error': 'indentation/bad'}
+            ]],
+            ['test_cases/c_bad_file.c', [
+                {'line': 5, 'error': 'indentation/bad'},
+                {'line': 6, 'error': 'indentation/bad'},
+                {'line': 7, 'error': 'indentation/bad'},
+                {'line': 9, 'error': 'indentation/bad'},
+            ]]
+        ]
+
+        for case in test_cases:
+            file = case[0]
+            expected = case[1]
+
+            f = open(file)
+            result = lint_c_or_cpp(f.read())
+            f.close()
+
+            self.assertEqual(len(result), len(expected), msg=result)
+
+            for got, exp in zip(result, expected):
+                self.assertEqual(got['line'], exp['line'])
+                self.assertEqual(got['error'], exp['error'])
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ src/linter/test_cases/c_almost_good_file.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    if (a > b) {
+       printf("TRUE");
+    } else {
+        printf("FALSE");
+    }
+}
