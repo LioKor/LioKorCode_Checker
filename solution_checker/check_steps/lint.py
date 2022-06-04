718a3222ec1c5fa6f3bc0204d78d7b0177127644
KoroLion <korolion31@yandex.ru>
2022-06-04 17:24:00 +0300

    refactoring

---
+++ solution_checker/check_steps/lint.py
@@ -0,0 +1,15 @@
+from linter.linter import lint_dict, lint_errors_to_str
+
+from solution_checker.models import LintResult
+import solution_checker.constants as c
+
+
+def lint_solution(source_code: dict) -> LintResult:
+    lint_errors = lint_dict(source_code)
+    str_lint = lint_errors_to_str(lint_errors)
+
+    lint_status = c.STATUS_OK if len(str_lint) == 0 else c.STATUS_LINT_ERROR
+    return LintResult(
+        status=lint_status,
+        message=str_lint
+    )
